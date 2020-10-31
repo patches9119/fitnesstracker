4189dbe2ca24572e506ad094c191d5dc105ddbe5
patches9119 <pbsanders1@gmail.com>
2020-10-31 16:12:41 -0700

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", {
   useNewUrlParser: true,
-  useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
 });
 
 // routes
